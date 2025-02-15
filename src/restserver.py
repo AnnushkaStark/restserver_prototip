5bbeade17d8e6b32e11fb61b925bdabdb9332806
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2025-02-16 02:35:23 +0300

    added runserver

---
+++ src/restserver.py
@@ -0,0 +1,19 @@
+from http.server import HTTPServer
+
+from handlers.main import RestserverHandler
+
+
+def run(
+    server_class=HTTPServer, handler_class=RestserverHandler, port: int = 8000
+) -> None:
+    server_adress = (" ", 8000)
+    http_server = server_class(server_adress, handler_class)
+    print(f"Rtarting server on port {port}")
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
+
+
+if __name__ == "__main__":
+    run()
